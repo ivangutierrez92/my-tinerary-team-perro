00a6116e68ef60785b69c5bdfa21ac44263a164c
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-02 16:17:56 -0300

    datos hoteles/shows

---
+++ src/data/shows.js
@@ -0,0 +1,155 @@
+let shows = [
+  {
+    id: "it1",
+    hotelId: "ho1",
+    name: "Gustavo Garay",
+    description: "Gustavo Garay, Argentinian violinist, arranger and composer.",
+    photo:
+      "https://ss-static-01.esmsv.com/id/156594/galeriaimagenes/obtenerimagen/?id=19&tipoEscala=stretch&width=570&height=856",
+    price: 70,
+    date: new Date(2022, 12, 20),
+    userId: "user1",
+  },
+
+  {
+    id: "it2",
+    hotelId: "ho1",
+    name: "Niki Sadeki",
+    description:
+      "Niki Sadeki is a Canadian DJ/producer based in New York. She is a self-taught, high-spirited creative, emboldened by the ideas, minds and sounds scattered",
+    photo:
+      "https://images.sk-static.com/images/media/img/col6/20210928-174657-927516.jpg",
+    price: 130,
+    date: new Date(2022, 10, 7),
+    userId: "user1",
+  },
+
+  {
+    id: "it3",
+    hotelId: "ho2",
+    name: "El Mundo",
+    description:
+      "Grammy Award–nominated early music ensemble El Mundo performs works from the Archivo de Guatemala: a treasure trove of 17th- and 18th-century music that highlights the unique confluence of sounds heard in the Guatemala City Cathedral. Regional folk music and dance rhythms of Spain, Africa, and the Americas meld with sacred compositions and courtly European elements in pieces by Castellanos, Falconieri, Murcia, Quiroz, and more. This historically and geographically distinctive music, called a “perfect balance of sacred and profane” (The Guardian), remains as vital and full of life as ever.",
+    photo:
+      "https://images.sk-static.com/images/media/profile_images/artists/431136/huge_avatar",
+    price: 98,
+    date: new Date(2022, 8, 20),
+    userId: "user2",
+  },
+
+  {
+    id: "it4",
+    hotelId: "ho2",
+    name: "Arctic Monkeys",
+    description:
+      "Grammy Award–nominated early music ensemble El Mundo performs works from the Archivo de Guatemala: a treasure trove of 17th- and 18th-century music that highlights the unique confluence of sounds heard in the Guatemala City Cathedral. Regional folk music and dance rhythms of Spain, Africa, and the Americas meld with sacred compositions and courtly European elements in pieces by Castellanos, Falconieri, Murcia, Quiroz, and more. This historically and geographically distinctive music, called a “perfect balance of sacred and profane” (The Guardian), remains as vital and full of life as ever.",
+    photo:
+      "https://images.sk-static.com/images/media/profile_images/artists/431136/huge_avatar",
+    price: 48,
+    date: new Date(2022, 3, 20),
+    userId: "user1",
+  },
+
+  {
+    id: "it5",
+    hotelId: "ho3",
+    name: "Green Day",
+    description:
+      "Green Day (formed in 1986) is a hugely popular and influential American punk rock band that have grown to become one of the most successful modern rock bands of their generation, hailing from East Bay, California",
+    photo:
+      "https://images.sk-static.com/images/media/img/col6/20190913-153414-812957.jpg",
+    price: 67,
+    date: new Date(2022, 7, 15),
+    userId: "user3",
+  },
+  {
+    id: "it6",
+    hotelId: "ho3",
+    name: "Blink-182",
+    description:
+      "Blink-182 is an American pop punk rock band from San Diego, California in 1992. The group has consisted of Mark Hoppus, Tom DeLonge, Travis Barker and Scott Raynor. The group has sold over 35 million albums worldwide",
+    photo:
+      "https://images.sk-static.com/images/media/img/col6/20190913-153414-812957.jpg",
+    price: 90,
+    date: new Date(2022, 7, 15),
+    userId: "user3",
+  },
+
+  {
+    id: "it7",
+    hotelId: "ho4",
+    name: "David Guetta",
+    description:
+      "David Guetta is a French dance-pop DJ and producer who has sold over 15 million singles worldwide. Guetta learned and perfected his craft in Paris nightclubs throughout the 1980s and 1990s.",
+    photo:
+      "https://images.sk-static.com/images/media/profile_images/artists/149066/huge_avatar",
+    price: 45,
+    date: new Date(2022, 5, 10),
+    userId: "user2",
+  },
+
+  {
+    id: "it8",
+    hotelId: "ho4",
+    name: "Drake",
+    description:
+      "Drake, full name Aubrey Drake Graham, is a Canadian rapper and actor from Toronto, Ontario. Born in 1986, Drake first rose to fame as an actor but was signed to Young Money Entertainment in 2009.",
+    photo:
+      "https://images.sk-static.com/images/media/img/col6/20180201-111734-971908.jpg",
+    price: 90,
+    date: new Date(2022, 1, 12),
+    userId: "user2",
+  },
+  {
+    id: "it9",
+    hotelId: "ho5",
+    name: "Calvin Harris",
+    description:
+      "Calvin Harris, real name Adam Richard Wiles, is a Scottish DJ and record producer known for his electropop disco sound, which has seen him surpass Michael Jackson as the UK record holder for most top ten hits from one …",
+    photo:
+      "https://images.sk-static.com/images/media/img/col6/20170815-222349-986117.jpg",
+    price: 55,
+    date: new Date(2022, 10, 3),
+    userId: "user3",
+  },
+
+  {
+    id: "it10",
+    hotelId: "ho5",
+    name: " The Offspring",
+    description:
+      "Formed in Garden Grove, California, The Offspring became hugely popular during their glory days of the '90s, with their anthemic, metal-tinged punk that helped reignite interest in the genre for a new generation",
+    photo:
+      "https://images.sk-static.com/images/media/img/col6/20210224-052612-976826.png",
+    price: 67,
+    date: new Date(2022, 10, 3),
+    userId: "user3",
+  },
+
+  {
+    id: "it11",
+    hotelId: "ho6",
+    name: "Slipknot",
+    description:
+      "Slipknot is an American heavy metal band from Des Moines, Iowa formed in 1995 by Shawn Crahan and Paul Gray. The band is well known for their elaborate masks and jumpsuits, which accompany their aggressively energetic",
+    photo:
+      "https://images.sk-static.com/images/media/profile_images/artists/1012485/huge_avatar",
+    price: 180,
+    date: new Date(2022, 10, 3),
+    userId: "user4",
+  },
+
+  {
+    id: "it12",
+    hotelId: "ho6",
+    name: "Red Hot Chili Peppers",
+    description:
+      "As one of the best-selling bands of all time with seven Grammys under their belt, Red Hot Chili Peppers are still going strong 30 years on from their debut album, and their arena gigs are something that I really think",
+    photo:
+      "https://images.sk-static.com/images/media/profile_images/artists/468146/huge_avatar",
+    price: 100,
+    date: new Date(2022, 6, 13),
+    userId: "user4",
+  },
+];
+export default shows; 
