613e76a2e869c33a98043f5b3149631432967993
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-03 11:26:03 -0400

    Se cambia NavBar para que tenga otro fondo al no ser la página principal

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/components/NavBar.css";
-import { Link as LinkRouter } from "react-router-dom";
+import { Link as LinkRouter, useLocation } from "react-router-dom";
 
 export default function Navbar() {
   const [hideNav, setHideNav] = useState(true);
   const [hideUser, setHideUser] = useState(true);
+  const [isHome, setIsHome] = useState(true);
+  let location = useLocation();
+  useEffect(() => {
+    if(location.pathname === "/") {
+      setIsHome(true);
+    } else {
+      setIsHome(false);
+    }
+  }, [location])
+
   const toggleHideUser = () => {
     setHideNav(true);
     setHideUser(!hideUser);
@@ -15,15 +25,15 @@ export default function Navbar() {
     setHideNav(!hideNav);
   };
   return (
-    <nav className="NavBar">
+    <nav className={`NavBar ${!isHome ? "bg-navbar" : ""}`}>
       <div className="NavBar-navigation">
         <LinkRouter to="/">
           <img className="NavBar-logo" alt="logo" src="./logo192.png" />
         </LinkRouter>
-        <button class="NavBar-menuButton" onClick={toggleHideNav}>
+        <button className="NavBar-menuButton" onClick={toggleHideNav}>
           <img src="./img/bx-menu.svg" alt="Menu Icon" />
         </button>
-        <div class={`NavBar-dropdown ${hideNav ? "NavBar-hide" : ""}`}>
+        <div className={`NavBar-dropdown ${hideNav ? "NavBar-hide" : ""}`}>
           <LinkRouter to="/cities">
             <button className="NavBar-link border-bottom-md-white border-round-md-top">Cities</button>
           </LinkRouter>
