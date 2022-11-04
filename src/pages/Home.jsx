72614921706a198c6bbd3658659f0ccfd7f887cb
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-04 10:18:16 -0400

    Agregado Home2 a Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Home1 from './Home1'
+import Home2 from './Home2'
 
 export default function Home() {
   return (
+    <>
     <Home1 />
+    <Home2 />
+    </>
   )
 }
