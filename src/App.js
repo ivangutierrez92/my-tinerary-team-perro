4e008458d640806f2efaefa64c7f1f6382ea7c05
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-04 18:40:30 -0300

    el estilo css de not found fue movido a styles/pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import "./styles/App.css";
 import NotFound from "./pages/NotFound";
-
+// import SignIn from "./pages/SignUp"
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
+        {/* <Routes path="/sigin" element={<SignIn />}/> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
