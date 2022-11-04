16d7d41eacdc678ee730c584c4a6a2aaf919f1d0
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-04 16:12:18 -0400

    Cartas renderizadas en bucle

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link as LinkRouter } from "react-router-dom";
+import "../styles/components/CityCard.css";
 
-export default function CityCard() {
+export default function CityCard({ city }) {
   return (
-    <div>CityCard</div>
-  )
+    <div className="CityCard">
+      <div className="CityCard-photo">
+      <LinkRouter to={`city/${city.id}`}>
+        <img src={city.photo} alt={city.name}></img>
+      </LinkRouter>
+      </div>
+      <div className="CityCard-body">
+        <h3 className="CityCard-title">{city.name}</h3>
+        <div className="CityCard-content">
+          <p>
+            <span className="CityCard-label">Continent:</span> {city.continent}
+          </p>
+          <p>
+            <span className="CityCard-label">Population:</span> {city.population.toLocaleString()}
+          </p>
+          </div>
+          <LinkRouter to={`city/${city.id}`}>
+            <button className="CityCard-button">Learn More</button>
+          </LinkRouter>
+      </div>
+    </div>
+  );
 }
--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,11 +1,19 @@
-import React from "react";
 import CityCard from "../components/CityCard";
 import Layout from "../layouts/Layout";
+import cities from "../data/cities";
+import '../styles/pages/Cities.css'
 
 export default function Cities() {
   return (
     <Layout>
-      <CityCard />
+      <div className="Cities">
+        <h1 className="Cities-title">Discover Cities Around The World</h1>
+        <div className="Cities-content">
+          {cities?.map(city => (
+            <CityCard city={city} key={city.id} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
