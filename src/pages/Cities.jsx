7059aa9d38e17b5e7fe82b3bff3b6b73856d31e2
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-04 14:04:27 -0400

    Agregado Layout a Cities

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import CityCard from "../components/CityCard";
+import Layout from "../layouts/Layout";
 
 export default function Cities() {
-  return <CityCard />;
+  return (
+    <Layout>
+      <CityCard />
+    </Layout>
+  );
 }
