9b8e17f9df50c92f4f076b57d195c7095d6b2577
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-04 10:31:37 -0400

    Agregado signup a App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import './styles/App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import "./styles/App.css";
+import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route index element={<Home />}/>
+        <Route index element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   );
