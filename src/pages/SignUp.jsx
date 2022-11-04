6e64d1cc2ff7a629c106ba787ecd00c71daa0f8a
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-04 11:24:56 -0400

    Modificado SignUp para que use componente SignUpForm

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import SignUpForm from "../components/SignUpForm";
 import Layout from "../layouts/Layout";
 import "../styles/pages/SignUp.css";
 
@@ -8,7 +9,7 @@ export default function SignUp() {
   let navigate = useNavigate();
   const sendData = event => {
     event.preventDefault();
-    
+
     let formObject = {};
     for (let input of formRef.current) {
       if (input.type !== "submit") {
@@ -28,47 +29,17 @@ export default function SignUp() {
     formRef.current.reset();
 
     alert("Thank for registering! you will be redirected soon ");
-    navigate('/')
-
+    navigate("/");
   };
   return (
     <Layout>
       <div className="SignUp">
         <h1 className="SignUp-title">Register to share your adventures!</h1>
         <hr />
-        <form className="SignUp-form" onSubmit={sendData} ref={formRef}>
-          <div className="SignUp-col">
-            <div className="SignUp-form-field">
-              <label htmlFor="name">Name:</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="SignUp-form-field">
-              <label htmlFor="lastName">Last Name:</label>
-              <input type="text" name="lastName" id="lastName" />
-            </div>
-          </div>
-          <div className="SignUp-col">
-            <div className="SignUp-form-field">
-              <label htmlFor="email">Email Address:</label>
-              <input type="email" name="email" id="email" />
-            </div>
-            <div className="SignUp-form-field">
-              <label htmlFor="password">Password:</label>
-              <input type="password" name="password" id="password" />
-            </div>
-          </div>
-          <div className="SignUp-col">
-            <div className="SignUp-form-field">
-              <label htmlFor="age">Age:</label>
-              <input type="number" name="age" id="age" />
-            </div>
-            <div className="SignUp-form-field"></div>
-          </div>
-
-          <input type="submit" value="Create account" className="SignUp-button" />
-          
-        </form>
-        <button className="Google-button"><img src="./img/g-logo.png" alt="google icon"></img>SIGN IN WITH GOOGLE</button>
+        <SignUpForm formRef={formRef} onSubmit={sendData} />
+        <button className="Google-button">
+          <img src="./img/g-logo.png" alt="google icon"></img>SIGN IN WITH GOOGLE
+        </button>
       </div>
     </Layout>
   );
