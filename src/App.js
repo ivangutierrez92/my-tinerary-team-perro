b72c28a647baf299b7e2c59b0b6df31a56595bd9
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-05 10:50:43 -0300

    sign in

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import "./styles/App.css";
 import NotFound from "./pages/NotFound";
-// import SignIn from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        {/* <Routes path="/sigin" element={<SignIn />}/> */}
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/signin" element={<SignIn/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
