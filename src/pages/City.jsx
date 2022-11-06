6b3b6ee77744cd61e2a075219f156b65fca7cf38
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-05 20:13:51 -0400

    Terminada página de City

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -2,29 +2,46 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Layout from "../layouts/Layout";
 import cities from "../data/cities";
-import '../styles/pages/City.css'
+import activities from "../data/activities";
+import "../styles/pages/City.css";
+import Itinerary from "../components/Itinerary";
 
 export default function City() {
-  let {city: cityId} = useParams();
-  let [city, setCity] = useState({})
+  let { city: cityId } = useParams();
+  let [city, setCity] = useState({});
+  let [itineraries, setItineraries] = useState([]);
+
   useEffect(() => {
-    let currentCity = cities.find((city) => city.id === cityId);
+    let currentCity = cities.find(city => city.id === cityId);
     setCity(currentCity);
-    console.log(cityId)
-    
+
+    let itinerariesArray = activities.filter(itinerary => itinerary.cityId === cityId);
+    setItineraries(itinerariesArray);
   }, [cityId]);
   return (
     <Layout>
-          <div className="City">
-            <div className="City-header">
-              <div className="City-image">
-                <img src={city?.photo} alt={city?.name} />
-              </div>
-              <div className="City-title">
-                <h1>{city?.name}</h1>
-              </div>
-            </div>
+      <div className="City">
+        <div className="City-header">
+          <div className="City-image">
+            <img src={city?.photo} alt={city?.name} />
           </div>
+          <div className="City-content">
+            <h1>{city?.name}</h1>
+            <p>{`${city?.name} is a beautiful place in ${city?.continent} `}</p>
+            <p>{`Here you can see the opinion of our users of what to do here`}</p>
+          </div>
+        </div>
+        {itineraries.length ? (
+          <>
+            <h2 className="City-itinerariesTitle">See what others are saying about the itineraries of the city</h2>
+            {itineraries.map(itinerary => (
+              <Itinerary itinerary={itinerary} />
+            ))}
+          </>
+        ) : (
+          <h2 className="City-itinerariesTitle">Couldn't found Itineraries for this city</h2>
+        )}
+      </div>
     </Layout>
   );
 }
