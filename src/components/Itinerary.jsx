cc9b47edd667a9716771db1e4f6bc9aeae9c08ef
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-11 18:14:34 -0300

    Se agrega traída desde API de ciudades e itinerarios

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -1,14 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../styles/components/Activity.css";
-import registeredUsers from "../data/registeredUsers";
 
 export default function Itinerary({ itinerary }) {
-  let [user, setUser] = useState("");
   let [isShowingComments, setIsShowingComments] = useState(false);
-  useEffect(() => {
-    let itineraryUser = registeredUsers.find(user => user.id === itinerary.userId);
-    setUser(itineraryUser);
-  }, [itinerary]);
 
   const showComments = () => {
     setIsShowingComments(!isShowingComments);
@@ -28,9 +22,6 @@ export default function Itinerary({ itinerary }) {
             <span className="bold">Duration:</span> {itinerary.duration} hrs.
           </p>
         </div>
-        <p>
-          Review By: {user?.name} {user?.lastName}
-        </p>
         <button className="Activity-button" onClick={showComments}>
           Comments
         </button>
