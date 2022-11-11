7e689e70c9357b0d6219dde36b64f113ca935967
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-11 18:15:55 -0300

    MT-02-24

--- src/components/DetailButton.jsx
+++ src/components/DetailButton.jsx
@@ -3,8 +3,14 @@ import { Link as LinkRouter } from "react-router-dom";
 import "../styles/components/detail-button.css"
 
 export default function HotelDetails({hotel}) {
+
+  
+
+
+
+
   return (
-    <LinkRouter to={`/hotel/${hotel}`}>
+    <LinkRouter to={`/hotel/name=${hotel.name}`}>
       <button className="CollectionCard-button">Details</button>
     </LinkRouter>
   );
