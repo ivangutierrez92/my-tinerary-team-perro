355523df3fa0bf0a05c333834a7131aa21080425
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-20 15:37:20 -0300

    MT-02-38

--- src/redux/reducers/hotelReducer.js
+++ src/redux/reducers/hotelReducer.js
@@ -1,7 +1,41 @@
 import { createReducer } from "@reduxjs/toolkit";
+import hotelActions from "../actions/hotelActions";
 
-const initialState = {};
+const { getHotelBefore,getHotelAfter } = hotelActions;
 
-const hotelReducer = createReducer(initialState, builder => {});
+const initialState = {
+  hotelList: [],
+  name:"",
+  order:"asc",
+  firstRender: true,
+};
+
+const hotelReducer = createReducer(initialState, (builder) => {
+  builder
+  .addCase(getHotelBefore.fulfilled, (state, action) => {
+    return {
+      ...state,hotelList: action.payload,firstRender: false,
+    };
+  })
+  .addCase(getHotelAfter.fulfilled, (state, action)=>{
+    return{
+    ...state,
+    hotelList: action.payload.data,
+    name:action.payload.name,
+    order:action.payload.order,
+    firstRender:false,
+
+
+    }
+
+
+
+
+  })
+
+
+
+
+});
 
 export default hotelReducer;
