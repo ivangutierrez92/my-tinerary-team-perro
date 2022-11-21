568ebb6c99042dc441793e01f376e1773d62514b
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-11-21 20:04:27 -0300

    Agregadas rutas dinámicamente, y cierre automático de menú al cambiar de página

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
 import "../styles/components/NavBar.css";
 import { Link as LinkRouter, useLocation } from "react-router-dom";
+import { routes, guestRoutes, userRoutes } from "../data/routes";
 
 export default function Navbar() {
   const [hideNav, setHideNav] = useState(true);
   const [hideUser, setHideUser] = useState(true);
   const [isHome, setIsHome] = useState(true);
+
   let location = useLocation();
   useEffect(() => {
-    if(location.pathname === "/") {
+    setHideUser(true);
+    setHideNav(true);
+    if (location.pathname === "/") {
       setIsHome(true);
     } else {
       setIsHome(false);
     }
-  }, [location])
+  }, [location]);
 
   const toggleHideUser = () => {
     setHideNav(true);
@@ -34,12 +38,11 @@ export default function Navbar() {
           <img src="/img/bx-menu.svg" alt="Menu Icon" />
         </button>
         <div className={`NavBar-dropdown ${hideNav ? "NavBar-hide" : ""}`}>
-          <LinkRouter to="/cities">
-            <button className="NavBar-link border-bottom-md-white border-round-md-top">Cities</button>
-          </LinkRouter>
-          <LinkRouter to="/hotels">
-            <button className="NavBar-link border-round-md-bottom">Hotels</button>
-          </LinkRouter>
+          {routes.map((route, index) => (
+            <LinkRouter to={route.link} key={`route-${index}`}>
+              <button className={route.className}>{route.name}</button>
+            </LinkRouter>
+          ))}
         </div>
       </div>
       <div className="NavBar-user">
@@ -48,24 +51,16 @@ export default function Navbar() {
         </button>
         {!hideUser && (
           <div className="user-buttons">
-            <LinkRouter to="/signin">
-              <button className="user-link border-bottom-white border-round-top">Sign in</button>
-            </LinkRouter>
-            <LinkRouter to="/signup">
-              <button className="user-link border-bottom-white">Sign up</button>
-            </LinkRouter>
-            <LinkRouter to="/newcity">
-              <button className="user-link border-bottom-white">New City</button>
-            </LinkRouter>
-            <LinkRouter to="/newhotel">
-              <button className="user-link border-bottom-white">New Hotel</button>
-            </LinkRouter>
-            <LinkRouter to="/mycities">
-              <button className="user-link border-bottom-white">My Cities</button>
-            </LinkRouter>
-            <LinkRouter to="/mytineraries">
-              <button className="user-link border-round-bottom">My Tineraries</button>
-            </LinkRouter>
+            {guestRoutes.map(route => (
+              <LinkRouter to={route.link}>
+                <button className={route.className}>{route.name}</button>
+              </LinkRouter>
+            ))}
+            {userRoutes.map(route => (
+              <LinkRouter to={route.link}>
+                <button className={route.className}>{route.name}</button>
+              </LinkRouter>
+            ))}
           </div>
         )}
       </div>
