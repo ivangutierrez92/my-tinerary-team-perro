6f765e7ff6c969b2054920c5cda3edc7a75a9430
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-27 11:50:17 -0300

    MT-02-54

--- src/pages/MyHotels.jsx
+++ src/pages/MyHotels.jsx
@@ -7,11 +7,15 @@ import myHotelsActions from "../redux/actions/myHotelsActions";
 import swal from "sweetalert";
 
 export default function MyHotels() {
+  let user = useSelector((store)=>store.signIn)
+  let { token } = useSelector((store) => store.signIn);
+  
   let { myHotelInit,myHotelDelete }= myHotelsActions;
   let {hotelList}=useSelector((store)=> store.myHotels)
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(myHotelInit({userId:"636d1ed3692e58acbf29845d"}));
+    console.log(user?.id)
+    dispatch(myHotelInit({userId:`${user.id}`}));
   }, []);
   console.log(hotelList);
   
@@ -24,7 +28,7 @@ const  deleteHotel = async(id) => {
       dangerMode: true,
     });
     if (answer) {
-      dispatch(myHotelDelete({ hotelId: id }));
+      dispatch(myHotelDelete({ hotelId: id,token:token}));
     }
   } catch (error) {
     swal("Something wrong",error.message,"error")
--- src/redux/actions/myHotelsActions.js
+++ src/redux/actions/myHotelsActions.js
@@ -4,20 +4,20 @@ import axios from "axios";
 const myHotelInit = createAsyncThunk("myHotelinit", async (data) => {
   try {
    const response= await axios.get(`${process.env.REACT_APP_API_URL}/api/hotels?userId=${data.userId}`)
-   console.log(response.data.response);
+   console.log(response.data);
    return response.data.response
   
   }catch(error){
     console.log(error);
-
+    return []
   }
 });
 
 const myHotelDelete= createAsyncThunk("myHotelDelete", async data => {
-
+   let headers = { headers: { Authorization: `Bearer ${data.token}` } };
   try{
 
-    const response= await axios.delete(`${process.env.REACT_APP_API_URL}/api/hotels/${data.hotelId}`)
+    const response= await axios.delete(`${process.env.REACT_APP_API_URL}/api/hotels/${data.hotelId}`, headers)
     console.log(response.data.id);
     return response.data.id
 
