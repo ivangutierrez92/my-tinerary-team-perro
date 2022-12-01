141cdb50245c060bff4192818cb7e380980510f4
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-12-01 13:00:22 -0300

    Agregada funcionalidad de quitar reacción

--- src/pages/MyReactions.jsx
+++ src/pages/MyReactions.jsx
@@ -3,10 +3,11 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import myReactionsActions from "../redux/actions/myReactionsActions";
 import reactionColors from "../data/reactionColors";
-import '../styles/pages/MyReactions.css'
+import "../styles/pages/MyReactions.css";
+import swal from "sweetalert";
 
 export default function MyReactions() {
-  let { getMyReactions } = myReactionsActions;
+  let { getMyReactions, deleteMyReaction } = myReactionsActions;
   let dispatch = useDispatch();
   let { id, token } = useSelector(store => store.signIn);
   let reactions = useSelector(store => store.myReactions);
@@ -14,22 +15,44 @@ export default function MyReactions() {
   useEffect(() => {
     dispatch(getMyReactions({ userId: id, token }));
   }, []);
+
+  const onDeleteReaction = async (reactionId, reactionName) => {
+    let res = await swal("Confirmation", "Are you sure that you want to delete this reaction?", {
+      buttons: ["Cancel", "Delete"],
+      dangerMode: true,
+    });
+    if (res) {
+      let dispatchResponse = await dispatch(deleteMyReaction({ reactionId, token, reactionName })).unwrap();
+      if (!dispatchResponse.success) {
+        swal("Error", dispatchResponse.message, "error");
+      }
+    }
+  };
+
   return (
     <div className="MyReactions">
       <h1 className="MyReactions-main-title">My Reactions</h1>
       {reactions &&
-        Object.keys(reactions).map(key => (
-          <>
-            <h2 className="MyReactions-title">{key}</h2>
-            {reactions[key].map(reaction => (
-              <div className="MyReactions-activity" style={{backgroundColor: reactionColors[key] || "black"}}>
-                <h3>{reaction.itineraryId.name}</h3>
-                <img className="MyReactions-activity-image" src={reaction.itineraryId.photo[0]} />
-                <button className="MyReactions-activity-button">X</button>
-              </div>
-            ))}
-          </>
-        ))}
+        Object.keys(reactions).sort().map(
+          key =>
+            reactions[key].length && (
+              <>
+                <h2 className="MyReactions-title">{key}</h2>
+                {reactions[key].map(reaction => (
+                  <div className="MyReactions-activity" style={{ backgroundColor: reactionColors[key] || "black" }}>
+                    <h3>{reaction.itineraryId.name}</h3>
+                    <img className="MyReactions-activity-image" src={reaction.itineraryId.photo[0]} />
+                    <button
+                      className="MyReactions-activity-button"
+                      onClick={() => onDeleteReaction(reaction._id, reaction.name)}
+                    >
+                      X
+                    </button>
+                  </div>
+                ))}
+              </>
+            )
+        )}
     </div>
   );
 }
--- src/redux/actions/myReactionsActions.js
+++ src/redux/actions/myReactionsActions.js
@@ -15,10 +15,26 @@ const getMyReactions = createAsyncThunk("getMyReactions", async data => {
   }
 });
 
+const deleteMyReaction = createAsyncThunk("deleteMyReaction", async data => {
+  let headers = { headers: { Authorization: `Bearer ${data.token}` } };
+  try {
+    let res = await axios.put(`${process.env.REACT_APP_API_URL}/api/reactions/${data.reactionId}`, null, headers);
+    return {
+      success: true,
+      id: res.data.id,
+      reactionName: data.reactionName,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: error.response.data.message || error.response.data || error.message,
+    };
+  }
+});
 
 const myReactionsActions = {
   getMyReactions,
-
+  deleteMyReaction,
 };
 
 export default myReactionsActions;
